2d54bf9225e2e4924626b391f38bf3598567954c
conno <D00233325@student.dkit.ie>
2022-11-29 21:03:26 +0000

    inital layout

--- Client/Client-TrainingGuruApp/components/structure/NavigationBar.js
+++ Client/Client-TrainingGuruApp/components/structure/NavigationBar.js
@@ -1,8 +1,48 @@
-function NavigationBar(){
+import {StyleSheet} from "react-native";
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome'
+import {faMugSaucer} from '@fortawesome/free-solid-svg-icons/faMugSaucer'
+import {faHouseChimney} from "@fortawesome/free-solid-svg-icons/faHouseChimney";
+import {faPeopleGroup} from "@fortawesome/free-solid-svg-icons/faPeopleGroup";
+import {faUser} from "@fortawesome/free-solid-svg-icons/faUser";
+
+function NavigationBar() {
+
+    const styles = StyleSheet.create({
+        listNav: {
+            fontSize: "1.2rem",
+            textTransform: "uppercase",
+            fontFamily: "Play",
+            fontWeight: "Bold",
+            flexDirection: "row",
+            alignContent: "space-around",
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "center",
+            gap: "2rem",
+
+            icon: {
+                width: "1.5rem",
+                height: "100%",
+                people: {
+                    width: "2.5rem"
+                }
+            }
+        }
+
+    })
     return <>
-        <div className="div">Home</div>
-        <div className="div">Clients</div>
-        <div className="div">Profile</div>
+        <div style={styles.listNav}>
+            <FontAwesomeIcon style={styles.listNav.icon} icon={faHouseChimney}/>
+            Home
+        </div>
+        <div style={styles.listNav}>
+            <FontAwesomeIcon style={{...styles.listNav.icon, ...styles.listNav.icon.people}} icon={faPeopleGroup}/>
+            Clients
+        </div>
+        <div style={styles.listNav}>
+            <FontAwesomeIcon  style={styles.listNav.icon} icon={faUser}/>
+            Profile
+        </div>
     </>
 
 }
