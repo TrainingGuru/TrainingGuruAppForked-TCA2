29a82f8bae0c19de7cd569265639e30bd7df7c59
conno <D00233325@student.dkit.ie>
2022-11-30 10:37:44 +0000

    setup styling system

--- Client/Client-TrainingGuruApp/components/structure/Layout.js
+++ Client/Client-TrainingGuruApp/components/structure/Layout.js
@@ -1,43 +1,85 @@
 import {Box, Container, Flex} from "native-base";
-import {StyleSheet} from "react-native";
 import NavigationBar from "./NavigationBar";
 import flex from "native-base/src/components/primitives/Flex/index";
-
-const styles = StyleSheet.create({
-    layout: {
-        height: "100%",
-        margin: "2%",
-        display: "flex",
-        flexDirection: "column",
-        base : {
-            display: "flex",
-            flexDirection: "row-reverse",
-            justifyContent: "flex-end",
-            header: {
-                height: "45%",
-                display: "flex",
-                alignContent: "center",
-                justifyContent: "center",
-                width: "100%",
-                img: {
-                    height: "100%"
-                }
-            },
-            navigation: {
-                display: "flex",
-                justifyContent: "space-around",
-                flexDirection: "column"
-            },
-        },
-        body: {
-            display: "flex",
-            flexDirection: "row",
-            marginLeft: "25%",
-        },
-    }
-})
+import StyleSheet from 'react-native-media-query';
+import CreateResponsiveStyle from "../../utils/responsiveStyle";
+import {Dimensions} from "react-native";
 
 function Layout({children}) {
+
+     let layout = {
+         width: Dimensions.get('window').width
+     }
+    const styles = CreateResponsiveStyle({
+        layout: {
+            height: "100%",
+            margin: "2%",
+            display: "flex",
+            flexDirection: "column",
+            base : {
+                display: "flex",
+                flexDirection: "row-reverse",
+                justifyContent: "flex-end",
+                header: {
+                    height: "45%",
+                    display: "flex",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    width: "100%",
+                    img: {
+                        height: "100%"
+                    }
+                },
+                navigation: {
+                    display: "flex",
+                    justifyContent: "space-around",
+                    flexDirection: "column"
+                },
+            },
+            body: {
+                display: "flex",
+                flexDirection: "row",
+                marginLeft: "25%",
+            },
+        }
+    }, {
+        layout: {
+            height: "100%",
+            margin: "2%",
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: "red",
+            base : {
+                display: "flex",
+                flexDirection: "row-reverse",
+                justifyContent: "flex-end",
+                header: {
+                    height: "45%",
+                    display: "flex",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    width: "100%",
+                    img: {
+                        height: "100%"
+                    }
+                },
+                navigation: {
+                    display: "flex",
+                    justifyContent: "space-around",
+                    flexDirection: "column"
+                },
+            },
+            body: {
+                display: "flex",
+                flexDirection: "row",
+                marginLeft: "25%",
+            },
+        }
+    }, layout)
+
+
+
+
     return <div style={styles.layout}>
         <div style={styles.layout.base}>
             <div style={styles.layout.base.header}>
---
+++ Client/Client-TrainingGuruApp/pages/client/ClientHome.js
@@ -0,0 +1,14 @@
+import View from "react-native-web/dist/vendor/react-native/Animated/components/AnimatedView";
+import Layout from "../../components/structure/Layout";
+import {StyleSheet} from "react-native";
+const styles = StyleSheet.create({})
+
+export const clientHome = () => {
+    return <Layout>
+                home
+            </Layout>
+
+
+}
+
+export default clientHome;
