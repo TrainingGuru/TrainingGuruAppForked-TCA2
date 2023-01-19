d50cf35e0692d4b5d8f18ca5cf7561af5fc6e22b
conno <D00233325@student.dkit.ie>
2023-01-19 11:38:56 +0000

    update styling

--- Client/Client-TrainingGuruApp/App.js
+++ Client/Client-TrainingGuruApp/App.js
@@ -3,15 +3,19 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Workouts from "./pages/client/Workouts";
-import {Image,StyleSheet} from "react-native";
+import {Image, View, StyleSheet} from "react-native";
 
 const Stack = createStackNavigator();
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: "red",
     },
     image: {
-        resizeMode: 'contain',
+        width: 90,
+        height: 70
     },
 });
 
@@ -20,27 +24,42 @@ function App() {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="Home" component={Home}   options={{
+                    headerTitleAlign: 'center',
                     headerTitle: () => (
-                        <Image
-                            style={styles.image}
-                            source={{ uri: "https://assets.api.uizard.io/api/cdn/stream/9789bb7f-8141-48f9-87dd-f2ebdadcbec6.png"}}
-                        />
-                    )
+                        <View style={styles.container}>
+                            <Image
+                                style={styles.image}
+                                source={{ uri: "https://assets.api.uizard.io/api/cdn/stream/9789bb7f-8141-48f9-87dd-f2ebdadcbec6.png"}}
+                            />
+                        </View>
+                    ),
+                    headerStyle: {
+                        borderBottomWidth: 0,
+                        height: "100%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        display: "flex",
+
+                    }
                 }} />
                 <Stack.Screen name="Profile" component={Profile}  options={{
                     headerTitle: () => (
-                        <Image
-                            style={styles.image}
-                            source={{ uri: "https://assets.api.uizard.io/api/cdn/stream/9789bb7f-8141-48f9-87dd-f2ebdadcbec6.png"}}
-                        />
+                        <View style={styles.container}>
+                            <Image
+                                style={styles.image}
+                                source={{ uri: "https://assets.api.uizard.io/api/cdn/stream/9789bb7f-8141-48f9-87dd-f2ebdadcbec6.png"}}
+                            />
+                        </View>
                     )
                 }} />
                 <Stack.Screen name="Workouts" component={Workouts} options={{
                     headerTitle: () => (
-                        <Image
-                            style={styles.image}
-                            source={{ uri: "https://assets.api.uizard.io/api/cdn/stream/9789bb7f-8141-48f9-87dd-f2ebdadcbec6.png"}}
-                        />
+                        <View style={styles.container}>
+                            <Image
+                                style={styles.image}
+                                source={{ uri: "https://assets.api.uizard.io/api/cdn/stream/9789bb7f-8141-48f9-87dd-f2ebdadcbec6.png"}}
+                            />
+                        </View>
                     )
                 }} />
             </Stack.Navigator>
