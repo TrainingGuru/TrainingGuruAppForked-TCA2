76d341be0bbf25c6ce61777e2f73bd9bb8f5b1c4
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-20 15:43:42 +0000

    index.js added headers options for API

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -12,6 +12,16 @@ const trainerRoute = require("./Routes/Trainer.js");
 app.get('/hello', (req, res) => {
     res.json('Hello from Server!!!')
 })
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use((_, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use("/User",userRoute);
 app.use("/Trainer",trainerRoute);
