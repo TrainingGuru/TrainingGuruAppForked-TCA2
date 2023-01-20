24aeb35105c83228e1e870687ddfd9b67f41ec51
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-20 12:02:03 +0000

    DB Connection File done

--- Backend/src/Config/DatabaseConfig.js
+++ Backend/src/Config/DatabaseConfig.js
@@ -1,1 +1,9 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('TrainingGuru', 'root', 'root', {
+    dialect: 'mysql',
+    host: 'localhost',
+});
+
+export default sequelize;
 
