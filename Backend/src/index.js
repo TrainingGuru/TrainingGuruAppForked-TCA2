c62def32f3bae61b9d27e5701428a1cec4459724
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-20 11:56:33 +0000

    Notes updated for installs

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,10 +3,13 @@ const HOSTNAME = '192.168.1.137' // change to yours (Might change daily so needs
 const express = require('express')
 const app = express()
 
+const userRoute = require("./Routes/User");
+
 //if visit http://192.168.1.215:8080/hello will know if server is working
 app.get('/hello', (req, res) => {
     res.json('Hello from Server!!!')
 })
+app.use("/user",userRoute);
 
 app.listen(PORT,HOSTNAME)
 console.log(`Server running at http://${HOSTNAME}:${PORT}`)
