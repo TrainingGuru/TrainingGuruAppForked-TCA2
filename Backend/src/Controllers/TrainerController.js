4d8e627d6b4f21ce5a07814e0695cf811b144fb4
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-21 16:34:06 +0000

    find user to see if email allready there

--- Backend/src/Controllers/TrainerController.js
+++ Backend/src/Controllers/TrainerController.js
@@ -2,7 +2,6 @@
 const Trainer = require("../Models/TrainersModel");
 
 //GetAllTrainer
-
 const getAllTrainers = async (req,res) =>{
     let trainers = await Trainer.findAll()
     if(trainers.length < 1){
@@ -11,6 +10,23 @@ const getAllTrainers = async (req,res) =>{
     else{
         res.status(200).send(trainers)
     }
+}
+
+const registerTrainer = async (req, res) => {
+
+    //Check email is not registered
+    Trainer.findOne({where : {
+        email: req.body.email,
+        }})
+        .then(userAccount => {
+            if(userAccount){
+                return res.status(409)
+            }
+            else{
+                res.status(200).json({message: 'no user'})
+            }
+        })
+
 }
 
 module.exports = {getAllTrainers}
