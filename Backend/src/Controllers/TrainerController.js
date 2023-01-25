1026c756f21520d27b57212bbfea8bd381a45c56
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-25 11:32:14 +0000

    todos added

--- Backend/src/Controllers/TrainerController.js
+++ Backend/src/Controllers/TrainerController.js
@@ -1,6 +1,7 @@
 const Trainer = require("../Models/TrainersModel");
 const {EmptyPacket} = require("mysql/lib/protocol/packets");
 
+//TODO:: VALIDATION in phase two
 //GetAllTrainer
 const getAllTrainers = async (req,res) =>{
     let trainers = await Trainer.findAll()
