4d60c2740022b83a79e6dc490d2767cdb709e305
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-25 16:27:49 +0000

    Client Login endpoint done

--- Backend/src/Controllers/ClientController.js
+++ Backend/src/Controllers/ClientController.js
@@ -41,5 +41,20 @@ const getAllClientsForTrainer = async (req,res) =>{
     })
 }
 
+const loginClient = async (req, res) => {
+
+    let clients = await Client.findOne({where : {
+            Email: req.body.Email,
+        }});
+
+    if(!clients){
+        res.status(404).send("No User found")
+    }else if(clients.Password == req.body.Password){
+        res.status(200).send("Login Details Valid")
+    }else{
+        res.status(401).send("Incorrect Password")
+    }
+}
+
 
 module.exports = {getAllClients,getAllClientsForTrainer}
