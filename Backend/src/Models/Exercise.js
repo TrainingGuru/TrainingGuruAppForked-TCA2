9485fae6be036e964d9a02633509169d56350399
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-26 11:27:18 +0000

    Excerise Model Created

---
+++ Backend/src/Models/Exercise.js
@@ -0,0 +1,38 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../Config/DatabaseConfig');
+
+const Exercise = sequelize.define("WorkOut",{
+    ExerciseID:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    Name:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    Type:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    Sets:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    Reps:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    Instructions:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+},{
+    tableName: 'Exercise',
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false
+});
+
+module.exports = Exercise;
