67051cadf7e7477a5d6e767d824bc33f16a2f29a
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-26 17:43:40 +0000

    commit before merger

--- Backend/src/Controllers/WorkOutsController.js
+++ Backend/src/Controllers/WorkOutsController.js
@@ -1,6 +1,9 @@
+const {Sequelize} = require("sequelize");
+
 const ClientWorkOut = require("../Models/ClientWorkoutModel");
 const PersonalBest = require("../Models/PersonalBestModel");
-const {Sequelize} = require("sequelize");
+const WorkOuts = require("../Models/TrainerWorkoutsModel");
+
 
 const Test = async (req,res) =>{
     let clients = await PersonalBest.findAll()
@@ -24,10 +27,30 @@ const WorkOutWeeks = async (req,res) => {
         attributes:[
             Sequelize.fn('DISTINCT',Sequelize.col('Week')),'Week']
         // include: [
-        //     {model: Workout}
+        //     {model: workout}
         // ]
     }).then(function (weekList){
-        if(weekList.length <= 0 || weekList == null){
+        if(weekList.length <= 0){
+            res.status(404).json("No Workouts Found");
+        }else{
+            res.status(200).json(weekList);
+        }
+    });
+}
+const GetWorkOutsForWeek = async (req,res) => {
+
+    await ClientWorkOut.findAll({
+        where : {
+            ClientID : req.params.id,
+            Week : req.params.wk
+        },
+        // attributes:[
+        //     Sequelize.fn('DISTINCT',Sequelize.col('Week')),'Week']
+        include: [
+            {model: WorkOuts}
+        ]
+    }).then(function (weekList){
+        if(weekList.length <= 0){
             res.status(404).json("No Workouts Found");
         }else{
             res.status(200).json(weekList);
@@ -35,10 +58,13 @@ const WorkOutWeeks = async (req,res) => {
     });
 }
 
+//const
+
 //Use able Methods in
 
 module.exports = {
     Test,
-    WorkOutWeeks
+    WorkOutWeeks,
+    GetWorkOutsForWeek
     
 }
--- Backend/src/Routes/WorkOuts.js
+++ Backend/src/Routes/WorkOuts.js
@@ -7,6 +7,7 @@ const router = express.Router();
 //GetAllTrainers
 router.get("/Workout",workoutController.Test);
 router.get("/:id/WorkoutWeeks",workoutController.WorkOutWeeks);
+router.get("/:id/Workouts/:wk",workoutController.GetWorkOutsForWeek);
 
 
 module.exports = router;
