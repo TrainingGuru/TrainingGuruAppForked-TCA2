1cba0e36017de9751e74837f395feaf2ae0901e6
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-26 13:57:04 +0000

    code done for NutritionHistory.js

--- Backend/src/Models/NutritionHistory.js
+++ Backend/src/Models/NutritionHistory.js
@@ -1,2 +1,36 @@
 const { Sequelize } = require('sequelize');
 const sequelize = require('../Config/DatabaseConfig');
+const Clients = require("./ClientModel");
+
+const NutritionHistory = sequelize.define('NutritionHistory',{
+    NutritionHistoryID:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    ClientID:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+            model: 'Client',
+            key: 'ClientID'
+        }
+    },
+    Date:{
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    CaloriesHit:{
+        type: sequelize.BOOLEAN,
+        allowNull: false
+    }
+},{
+    tableName: 'Goals',
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false
+})
+NutritionHistory.belongsTo(Clients,{foreignKey: 'ClientID'});
+Clients.hasMany(NutritionHistory,{foreignKey: 'ClientID'})
+module.exports = NutritionHistory;
