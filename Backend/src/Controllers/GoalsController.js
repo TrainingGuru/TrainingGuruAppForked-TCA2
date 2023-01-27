5670571aad193badabd4eb50350e7dc55b6a9846
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-27 18:52:08 +0000

    goals route and controller created

---
+++ Backend/src/Controllers/GoalsController.js
@@ -0,0 +1,13 @@
+const {Sequelize} = require("sequelize");
+
+const getAllClients = async (req,res) =>{
+    let clients = await Client.findAll()
+    if(clients.length < 1){
+        res.status(404)
+        res.end()
+    }
+    else{
+        res.status(200).send(clients)
+        res.end();
+    }
+}
