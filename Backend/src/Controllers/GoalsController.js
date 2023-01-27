c71e127aba675cd2d94c8cb09c88c620cac50348
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-27 19:08:00 +0000

    getAllGoalsForClient() created ready for test

--- Backend/src/Controllers/GoalsController.js
+++ Backend/src/Controllers/GoalsController.js
@@ -3,15 +3,18 @@ const {Sequelize} = require("sequelize");
 const Goals = require("../Models/GoalsModel");
 
 const getAllGoalsForClient = async (req,res) =>{
-    let clients = await Goals.findAll()
+    let goals = await Goals.findAll({
+        where : {
+            ClientID : req.params.id
+        }
+    })
 
-    if(clients.length < 1){
-        res.status(404)
+    if(goals.length <=0){
+        res.status(404).json(`Client ${req.params.id} has no Saved Goals`)
         res.end()
     }
     else{
-        res.status(200).send(clients)
-        res.end();
+        res.status(200).json(goals)
     }
 }
 
