34a2db9aef141fa43763bd63698f3eff147c3b69
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-27 18:53:36 +0000

    goals route basic code

---
+++ Backend/src/Routes/Goals.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const goalController = require('../Controllers/GoalsController')
+
+
+const router = express.Router();
+
+//GetAllTrainers
+router.get("/")
+
+
+
+module.exports = router;
