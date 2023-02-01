597546d2901deb74e481e7d9bbe438d1bc83dc56
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-02-01 18:04:24 +0000

    default / route added

--- Backend/src/Config/DatabaseConfig.js
+++ Backend/src/Config/DatabaseConfig.js
@@ -1,5 +1,13 @@
 const { Sequelize } = require('sequelize');
 
+
+// const database = new Sequelize('TrainingGuru', 'admin', 'TrainingGuru', {
+//     dialect: 'mysql',
+//     host: 'traininggurudbonline.cxnpilaou3nh.eu-west-1.rds.amazonaws.com',
+//     port: '3306'
+//
+// });
+
 const database = new Sequelize('TrainingGuru', 'root', 'root', {
     dialect: 'mysql',
     host: 'localhost',
